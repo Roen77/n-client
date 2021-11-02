3cac2931934eba1b3300ec4ba653067e2ba5e6ff
Taegyeong Han <65719512+Ronis-han@users.noreply.github.com>
2021-11-03 02:40:38 +0900

    change haed meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,8 +29,6 @@ export default {
       { src: 'https://kit.fontawesome.com/0c7f47858a.js' }
     ]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/static/css/common.css',
     '@/static/css/style.css'
@@ -39,32 +37,22 @@ export default {
     color: '#5C6BC0',
     height: '5px'
   },
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins' },
     { src: '~/plugins/axios' }
   ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     '@nuxtjs/moment'
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios'
   ],
   bootstrapVue: {
-    bootstrapCSS: true, // Or `css: false`
-    bootstrapVueCSS: true, // Or `bvCSS: false`
+    bootstrapCSS: true,
+    bootstrapVueCSS: true,
     components: ['BPagination'],
     componentPlugins: [
       'FormDatepickerPlugin'
@@ -74,15 +62,11 @@ export default {
     defaultLocale: 'ko',
     locales: ['ko']
   },
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     browserBaseURL: process.env.NODE_ENV === 'production' ? 'https://app.roen.pe.kr/' : 'http://localhost:5000/',
     baseURL: process.env.NODE_ENV === 'production' ? 'https://app.roen.pe.kr/' : 'http://localhost:5000/',
-    // baseURL: 'http://localhost:5000/',
     credentials: true
   },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
   server: {
